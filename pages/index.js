5bab8f18f1dbc743a750e2dd602aba4dc6f494ad
wasiq <wasiq_nauman@hotmail.com>
2021-09-11 19:41:26 -0400

    Front page complete

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard';
 import MediumCard from '../components/MediumCard';
 import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
 export default function Home( { exploreData, cardsData }) {
   return (
@@ -38,13 +39,15 @@ export default function Home( { exploreData, cardsData }) {
             ))}
           </div>
         </section>
-        
-        <LargeCard img="https://links.papareact.com/4cj"
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
           title="The Greatest Outdoors"
           description="Wishlists curated by Airbnb."
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   );
 }
