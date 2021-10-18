9298a94705abd53a606b20ea50036dc664419148
Wasiq <wasiq@Wasiqs-MacBook-Pro.local>
2021-10-18 10:18:25 -0400

    Added calender to Header, so that it shows when a user types something in the search field

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,28 @@ import {
   UserCircleIcon,
   UsersIcon
 } from '@heroicons/react/solid'
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
 
 function Header() {
+  // use state to get search input value
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date);
+  const [endDate, setEndDate] = useState(new Date);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection'
+  };
     return (
       <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
         {/* LEFT */}
@@ -20,20 +40,32 @@ function Header() {
           />
         </div>
         {/* MIDDLE - SEARCH*/}
-        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
-            <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"></input>
-            <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>        
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+            <input 
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"></input>
+            <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 hover:shadow-md hover:scale-103 transition duration-300"/>        
         </div>
         {/* RIGHT */}
-        <div className="flex items-center space-x-4 justify-end text-gray-500">
-          <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
-          <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-4 justify-end">
+          <p className="hidden md:inline-flex cursor-pointer transition duration-300 text-gray-600 hover:text-gray-900">Become a host</p>
+          <GlobeAltIcon className="h-6 cursor-pointer text-gray-600 transition duration-300 hover:text-gray-900" />
           {/* Menu button */}
-          <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <div className="flex items-center space-x-2 border-2 p-2 rounded-full text-gray-600 transition duration-300 hover:shadow-md hover:text-gray-900">
             <MenuIcon className="h-6" />
             <UserCircleIcon className="h-6"/>
           </div>
         </div>
+
+        {searchInput && <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}/>
+          </div>}
+
       </header>
     );
 }
